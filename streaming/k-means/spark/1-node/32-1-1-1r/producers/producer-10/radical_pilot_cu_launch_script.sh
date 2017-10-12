ed277b8f3fc8d90caf7f9702d69e1278ab051a87
George Chantzialexiou <tg829618@login1.wrangler.tacc.utexas.edu>
2017-10-12 15:15:31 -0500

    update data

---
+++ streaming/k-means/spark/1-node/32-1-1-1r/producers/producer-10/radical_pilot_cu_launch_script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+# Environment variables
+export RP_SESSION_ID="rp.session.one.george.017451.0004"
+export RP_PILOT_ID="pilot.0000"
+export RP_AGENT_ID="agent_0"
+export RP_SPAWNER_ID="agent.executing.0.child"
+export RP_UNIT_ID="unit.000009"
+export RP_GTOD="/work/03662/tg829618/wrangler/radical.pilot.sandbox/rp.session.one.george.017451.0004/pilot.0000/gtod"
+export RP_PROF="/work/03662/tg829618/wrangler/radical.pilot.sandbox/rp.session.one.george.017451.0004/pilot.0000/unit.000009/PROF"
+export RP_TMP="None"
+
+
+touch $RP_PROF
+
+# Change to unit sandbox
+cd /work/03662/tg829618/wrangler/radical.pilot.sandbox/rp.session.one.george.017451.0004/pilot.0000/unit.000009
+
+# The command to run
+python "data_producer.py" "c251-108:9092" 
+RETVAL=$?
+
+# Exit the script with the return code from the command
+exit $RETVAL
